569bebe34cf4ae26bb2641caecead3eaa842e720
Danush Ramanan <dan.ramanan@gmail.com>
2025-10-05 11:09:52 -0600

    command line almost works

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const input = document.querySelector("#realInput");
+const mirror = document.querySelector("#mirror");
+const caret = document.querySelector("#caret");
+
 async function loadFiles(path = "cdn") {
     const res = await fetch(`https://api.github.com/repos/duck123acb/duck123acb.github.io/contents/${path}`);
     const files = await res.json();
@@ -12,6 +16,24 @@ async function loadFiles(path = "cdn") {
     });
 }
 
-loadFiles();
+function updateCaret(e) {
+    if (e?.key && e.key.length !== 1) return;
 
-//TODO: setup HTML from loading the files
+    const textBeforeCaret = input.value.substring(2, input.selectionStart);
+    const formattedText = "$ " + textBeforeCaret.replace(/ /g, "\u00a0");
+    mirror.textContent = formattedText;
+    input.value = formattedText;
+
+    const rect = mirror.getBoundingClientRect();
+    caret.style.left = rect.width + 2 + "px";
+    caret.style.top = rect.height + "px";
+}
+
+// loadFiles();
+
+input.addEventListener("input", updateCaret);
+input.addEventListener("click", updateCaret);
+input.addEventListener("keyup", updateCaret);
+updateCaret();
+
+input.focus();
